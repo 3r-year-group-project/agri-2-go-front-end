2d869ebd70d8cde141b8c8563cced0a9d4aa68c8
dilshanSankalpa <dilshansankalpa98@gmail.com>
2022-08-01 01:24:18 +0530

    clear error : parse error

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from './services/utils/theme';
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from 'axios';
-import {setApiEndPoint} from './services/utils/axios';
-import { setApiEndPoint } from './services/utils/api/index';
+import { setApiPath } from './services/utils/api/index';
 
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
   const [userType, setUserType] = useState(0);
   useEffect(()=>{
     if (isAuthenticated) {
-      axios.get(setApiEndPoint(`/user?userId=${user.user_id}`)).then(res =>{
+      axios.get(setApiPath(`/user?userId=${user.user_id}`)).then(res =>{
           setUserType(res.userType);
           /**
            * TODO: add codes what needs to be done after user type is found
--- src/services/utils/api/index.js
+++ src/services/utils/api/index.js
@@ -1,9 +1,10 @@
 /***
+import setApiEndPoint from './index';
  * @abstract provide completer API path
  * @param {string dest} routes of the api 
  * @return {string} completed path to the routes
  *  
 */
-export const setApiEndPoint = (dest) => {
+export function setApiPath(dest){
     return "localhost:3002/api" + dest;
 }

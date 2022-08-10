66771cb68277f45a74070948af6f5c45d91b59fe
Venushka <venushkachstc@gmail.com>
2022-08-11 00:15:11 +0530

    created a new car comoponent with grid structure for the find new vegetables tab and populated it with data

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -20,7 +20,7 @@ import ContinueRegistration from "../pages/ContinueRegistration";
 export default function Router() {
 
   const isAuthenticated='1';
-  const userRole='2';
+  const userRole='6';
 
   // admin=1
   // customer=2

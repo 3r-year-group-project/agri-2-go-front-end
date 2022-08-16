b85e940fac662a8d27d85ada79e7a92bbe0c8908
ChathuraAlwis <chathura7alwis@gmail.com>
2022-08-17 00:16:40 +0530

    corrected error (import react)

--- src/pages/Gardener/Transactions/index.jsx
+++ src/pages/Gardener/Transactions/index.jsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import Box from '@mui/material/Box';
 import FormLabel from '@mui/material/FormLabel';
 import TransactionTable from "../../../components/Transactions";

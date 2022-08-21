17bbc2fcd508a6f22b0cfa03d767c38e7197a2a1
SethniBhagya <sethnibhagya67@gmail.com>
2022-08-21 19:09:25 +0530

    Add delete button to the Subscription details page

--- src/pages/Admin/Subscriptions/subscription.jsx
+++ src/pages/Admin/Subscriptions/subscription.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { useState } from "react";
-import { Container, Row, Col, Button, Form } from "react-bootstrap";
+
+import { Container } from '@mui/material';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
@@ -11,15 +12,23 @@ import {FormPropsTextFields2} from './content'
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import { borderRadius, padding } from '@mui/system';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import Stack from '@mui/material/Stack';
 
 export default function Sub() {
   return (
     <React.Fragment>
     <CssBaseline />
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
       <Box sx={{ bgcolor: 'white', height: 'auto', color:'black', borderRadius:'10px 10px 10px 10px' }} >
         <Box sx={{ bgcolor: '#075E54', height: 'auto', color:'white', padding:'15px', borderRadius:'10px 10px 0 0' }}>
-         <Typography ><b>Basic</b></Typography>
+        <Stack spacing={2} direction="row">
+         <Typography  variant="h5" >Basic</Typography><Button variant="contained" color="error" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      </Stack>
         </Box>
         <Box sx={{ bgcolor: 'white',  padding:'10px 20px 20px 10%',height: 'auto', color:'black', borderRadius:'0 0 10px 10px' }}>
         <FormPropsTextFields/>
@@ -28,17 +37,33 @@ export default function Sub() {
       </Box>
     </Container>
    
-    <Container maxWidth="sm" >
+    <Container maxWidth="md" >
       <Box sx={{ bgcolor: 'white', height: 'auto', color:'black', borderRadius:'10px 10px 10px 10px', marginTop:'20px' }} >
         <Box sx={{ bgcolor: '#075E54', height: 'auto', color:'white', padding:'15px', borderRadius:'10px 10px 0 0' }}>
-         <Typography ><b>Standard</b></Typography>
+        <Stack spacing={2} direction="row">
+         <Typography  variant="h5" >Standard</Typography><Button variant="contained" color="error" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+      </Stack>
         </Box>
         <Box sx={{ bgcolor: 'white',  padding:'10px 20px 20px 10%',height: 'auto', color:'black', borderRadius:'0 0 10px 10px' }}>
         <FormPropsTextFields2/>
         </Box>
 
       </Box>
+
     </Container>
+    <Container maxWidth="md" >
+      <Box sx={{ bgcolor: 'none', height: 'auto', color:'black', borderRadius:'10px 10px 10px 10px', marginTop:'20px' }} >
+      <Button variant="contained" startIcon={<AddCircleIcon/>} color="success">
+        ADD NEW PLAN
+      </Button>
+    
+
+      </Box>
+
+    </Container>
+    
   </React.Fragment>
 
   );

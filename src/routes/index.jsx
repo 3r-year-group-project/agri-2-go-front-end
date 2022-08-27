c25cae0db293747638898ef07e65916e4112d454
Venushka <venushkachstc@gmail.com>
2022-08-27 23:43:10 +0530

    removed functionalities of grocery seller

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,7 +6,6 @@ import CustomerLayout from "../layouts/CustomerLayout";
 import FarmerLayout from "../layouts/FarmerLayout";
 import GardenerLayout from "../layouts/GardnerLayout";
 import StockBuyerLayout from "../layouts/StockBuyerLayout";
-import GrocerySellerLayout from "../layouts/GrocerySellerLayout";
 import TransporterLayout from "../layouts/TransporterLayout";
 import WastageRecyclecenterLayout from "../layouts/WastageRecycleCenterLayout";
 import CategoryPage from "../pages/CategoryPage";
@@ -28,7 +27,6 @@ import PageNotFound from "../pages/PageNotFound";
 import FarmerRegistrationForm from "../components/RegistrationForms/Farmer";
 import GardenerRegistrationForm from "../components/RegistrationForms/Gardener";
 import StockBuyerRegistrationForm from "../components/RegistrationForms/StockBuyer";
-import GrocerySellerRegistrationForm from "../components/RegistrationForms/GrocerySeller";
 import CustomerRegistrationForm from "../components/RegistrationForms/Customer";
 import WastageRecycleCenterRegistrationForm from "../components/RegistrationForms/WastageRecycleCenter";
 import TransporterRegistrationForm from "../components/RegistrationForms/Transporter";
@@ -78,7 +76,6 @@ export default function Router() {
         <Route path="/register/farmer/:id" element={<FarmerRegistrationForm/>}/>
         <Route path="/register/gardener/:id" element={<GardenerRegistrationForm/>}/>
         <Route path="/register/stockbuyer/:id" element={<StockBuyerRegistrationForm/>}/>
-        <Route path="/register/groceryseller/:id" element={<GrocerySellerRegistrationForm/>}/>
         <Route path="/register/customer/:id" element={<CustomerRegistrationForm/>}/>
         <Route path="/register/wastagerecyclecenter/:id" element={<WastageRecycleCenterRegistrationForm/>}/>
         <Route path="/register/transporter/:id" element={<TransporterRegistrationForm/>}/>
@@ -96,7 +93,6 @@ export default function Router() {
         <Route path="/farmer/dash/:page" element={<FarmerLayout/>}/>
         <Route path="/gardener/dash/:page" element={<GardenerLayout/>}/>
         <Route path="/stockbuyer/dash/:page" element={<StockBuyerLayout/>}/>
-        <Route path="/groceryseller/dash/:page" element={<GrocerySellerLayout/>}/>
         <Route path="/transporter/dash/:page" element={<TransporterLayout/>}/>
         <Route path="/wastagerecyclecenter/dash/:page" element={<WastageRecyclecenterLayout/>}/>
         <Route path="/test" element={<Review />}/> 

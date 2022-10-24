496254b617944db39ab2c5dab51b2eab5950bc2d
Chathura Alwis <chathura7alwis@gmail.com>
2022-10-24 20:46:19 +0530

    Backend for ViewPrices

--- src/pages/Farmer/ViewPrices/chart.jsx
+++ src/pages/Farmer/ViewPrices/chart.jsx
@@ -14,112 +14,11 @@ import theme from '../../../services/utils/theme'
 import { color } from '@mui/system';
 import { Typography } from '@mui/material';
 
-// Generate Sales Data
-function createData(date, amount) {
-  return { date, amount };
-}
-
-const data = [
-
-  createData('04 Aug', 300),
-  createData('05 Aug', 600),
-  createData('06 Aug', 300),
-  createData('07 Aug', 150),
-  createData('08 Aug', 200),
-  createData('09 Aug', 400),
-  createData('10 Aug', 400),
-  createData('11 Aug', 200),
-];
-
-const data1 = [
-
-  createData('04 Aug', 200),
-  createData('05 Aug', 100),
-  createData('06 Aug', 300),
-  createData('07 Aug', 550),
-  createData('08 Aug', 100),
-  createData('09 Aug', 600),
-  createData('10 Aug', 200),
-  createData('11 Aug', 800),
-];
-
-const pdata = [
-  {
-      Date: '04 Aug',
-      MinimumPrice: 11,
-      MaximumPrice: 120
-  },
-  {
-      Date: '05 Aug',
-      MinimumPrice: 15,
-      MaximumPrice: 12
-  },
-  {
-      Date: '06 Aug',
-      MinimumPrice: 5,
-      MaximumPrice: 10
-  },
-  {
-      Date: '07 Aug',
-      MinimumPrice: 10,
-      MaximumPrice: 5
-  },
-  {
-      Date: '08 Aug',
-      MinimumPrice: 9,
-      MaximumPrice: 4
-  },
-  {
-      Date: '09 Aug',
-      MinimumPrice: 10,
-      MaximumPrice: 8
-  },
-  {
-    Date: '10 Aug',
-    MinimumPrice: 12,
-    MaximumPrice: 140
-  },
-  {
-    Date: '11 Aug',
-    MinimumPrice: 110,
-    MaximumPrice: 12
-},
-{
-    Date: '12 Aug',
-    MinimumPrice: 150,
-    MaximumPrice: 220
-},
-{
-    Date: '13 Aug',
-    MinimumPrice: 50,
-    MaximumPrice: 100
-},
-{
-    Date: '14 Aug',
-    MinimumPrice: 120,
-    MaximumPrice: 500
-},
-{
-    Date: '15 Aug',
-    MinimumPrice: 90,
-    MaximumPrice: 400
-},
-{
-    Date: '16 Aug',
-    MinimumPrice: 100,
-    MaximumPrice: 180
-},
-{
-  Date: '17 Aug',
-  MinimumPrice: 120,
-  MaximumPrice: 140
-},
-  
-];
-
-
 export default function Chart(props) {
- 
+
+  props.data.map((row) => {
+    row.Date = row.Date.substring(0, 10)
+  })
 
   return (
     <React.Fragment>
@@ -129,7 +28,7 @@ export default function Chart(props) {
       </Typography>
       <br/>
       <ResponsiveContainer>
-      <LineChart data={pdata}>
+      <LineChart data={props.data}>
                     <CartesianGrid />
                     <XAxis dataKey="Date" 
                         interval={'preserveStartEnd'} />

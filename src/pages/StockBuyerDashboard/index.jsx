dc945bbf46c87dd4823221fa946266dc61541a50
Chathura Alwis <chathura7alwis@gmail.com>
2022-10-24 14:28:52 +0530

    StockBuyerDashboard error corrected

--- src/pages/StockBuyerDashboard/index.jsx
+++ src/pages/StockBuyerDashboard/index.jsx
@@ -1,7 +1,7 @@
 import { Typography } from '@mui/material'
 import React from 'react'
 import axios from "axios";
-import DashboardContent from '../../../components/Dashboard/dashboard'
+import DashboardContent from '../../components/Dashboard/dashboard'
 
 
 

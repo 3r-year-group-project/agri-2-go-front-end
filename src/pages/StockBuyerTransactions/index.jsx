d8cc19d2adea87b8fe6517ff5c6cedb63aabb2f5
Venushka <venushkachstc@gmail.com>
2022-10-24 13:01:02 +0530

    Added the wastage price details and fixed the table loading issue in stocks table in stock buyers side

--- src/pages/StockBuyerTransactions/index.jsx
+++ src/pages/StockBuyerTransactions/index.jsx
@@ -2,22 +2,48 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import FormLabel from '@mui/material/FormLabel';
 import TransactionTable from "../../components/Transactions";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function createData(date, description, amount, status) {
     return { date, description, amount, status };
 }
   
-const rows = [
+let rows = [
     
     
-    createData('2022.08.27', "Kamal Fernando", "1600.00", "+ Pending"),  
-    createData('2022.08.29', "Lakmal Silva", "1600.00", "- Completed"),
-    createData('2022.08.30', "Nelum Peries", "3200.00", "- Pending")
+    // createData('2022.08.27', "Kamal Fernando", "1600.00", "+ Pending"),  
+    // createData('2022.08.29', "Lakmal Silva", "1600.00", "- Completed"),
+    // createData('2022.08.30', "Nelum Peries", "3200.00", "- Pending")
     
     
 ].sort((a, b) => (a.date < b.date ? -1 : 1));
 
 export default function GardenerTransactions() {
+
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    React.useEffect(() => {
+        console.log("Running Transactions!!!!!!!!!!");
+        axios.post('/api/stockbuyer/transactions/getdetails',{email:user.email})
+        .then(res => {
+          console.log("jhsyagfduyadsg")
+          rows = []
+          
+          for(let i=0;i<res.data.data.length;i++){
+            rows.push(createData(res.data.data[i].date_time,res.data.data[i].first_name.concat(" ",res.data.data[i].last_name),res.data.data[i].min_advance,res.data.data[i].status));  
+          
+          
+          console.log(res.data.data[i])
+          }
+        console.log(res.data.data)
+          
+        })        
+        
+      }, []);
+
+
+
     return(
         <div style={{ background: 'rgba(37, 211, 102, 0.2)', padding:'5%',minHeight:'100%'}} >
             <Box m={2} sx={{paddingLeft: 3, paddingRight: 3}}>
